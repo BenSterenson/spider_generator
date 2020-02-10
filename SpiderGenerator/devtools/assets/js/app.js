64a7cf61c0be7944f24782f1d95b0661b51e3206
bensterenson <ben@cybersixgill.com>
2020-02-10 20:01:08 +0200

    working with vue-material

--- SpiderGenerator/devtools/assets/js/app.js
+++ SpiderGenerator/devtools/assets/js/app.js
@@ -1,6 +1,15 @@
 import Vue from 'vue';
 import App from './components/App/app.vue';
 
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
+Vue.use(MdButton);
+Vue.use(MdContent);
+Vue.use(MdTabs);
+
+
 const app = new Vue({
     el: '#my-app',
     components: {
